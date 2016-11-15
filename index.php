ea14106b8626802b575a5218df70059e505d16fd
tamaskelemen <tamaskelemen.kt@gmail.com>
2016-11-16 00:01:02 +0100

    added shits

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+###########################################################################################
+			#	  ###		 #	  #  #######  #       #     #
+			#	 #   #		 #	  #  #		  #        #   #
+			#	#     # 	 ######  #######  #	        ###
+			#	#	  #		 #	  #  #		  #	         #
+			#	 #	 #		 #	  #	 #		  #	    	 #
+			#	  ###   #    #	  #	 #######  ######	 #     
+############################################################################################			
+			
+			
+			?>
+<html>
+<head>
+<title>ötödik a tizedik olimpia</title>
+	<meta http-equiv="Content-Type" content="text/html; "charset = 'utf-8'>
+	<link rel="stylesheet" type="text/css" href="style.css" media="all"/>
+</head>
+
+<body>
+	<!-- banner .... ......................................................................................................................... -->
+	<div class = "banner">
+		<a href= 'index.php'><img class='img' src='pizza.jpg'></a>
+	</div>
+	<!-- banner vége........................................................................................................................... -->	
+	<!-- menu .................................................................................................................................. -->
+	<div class="menu">		
+		<a class="link" href="index.php">	<div class = 'menu-pont'  >Home </div>	</a>
+		<a class="link" href="index.php?menu=faq">	<div class = 'menu-pont'  >	FAQ	</div>	</a>
+		<a class="link" href="index.php?menu=hamburger">	<div class = 'menu-pont'  >	Hamburgerek	</div></a>
+		<a class="link" href="index.php?menu=kosar"> 	<div class = 'menu-pont'  >	Kosár	</div></a>
+		<a class="link" href="index.php?menu=contact">	 <div class = 'menu-pont'  >Kapcsolat	</div></a>		
+	</div>
+	<!-- Menu vege ..........................................................................................................................-->
+	
+</body>
+
+<html>
